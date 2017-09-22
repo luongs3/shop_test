66dad913be7c1dd64602b747b3cf273f98d8c741
james nguyen <luongr3@gmail.com>
2017-09-22 16:48:26 +0700

    init vue component

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin - Index</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/admin.css') }}">
+</head>
+<body>
+    <div id="app">
+        <div class="loading">
+            <div class="splash-overlay"></div>
+            <div class="splash-spinner"></div>
+        </div>
+    </div>
+    <div class="splash-screen">
+        <div class="splash-overlay"></div>
+        <div class="splash-spinner"></div>
+    </div>
+    <script type="text/javascript" src="{{ mix('js/admin.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,79 @@
 <?php
+//Client
+Route::group(['domain' => env('CLIENT_DOMAIN')], function() {
+    Route::get('/', function () {
+        return view('teacher.index');
+    });
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    Route::group(['namespace' => 'Client'], function () {
+        Route::get('auth', 'Auth\SessionsController@index')->name('teacher.auth');
+        Route::post('login', 'Auth\SessionsController@store')->name('teacher.login');
+        Route::get('logout', 'Auth\SessionsController@logout')->name('teacher.logout');
 
-Route::get('/', function () {
-    return view('welcome');
+        Route::post('avatar', 'Auth\ProfileController@updateAvatar');
+        Route::post('background', 'Auth\ProfileController@updateBackground');
+        Route::get('show/{teacher?}', 'Auth\ProfileController@show')->name('teacher.show');
+        Route::post('update', 'Auth\ProfileController@updateProfile')->name('teacher.update');
+        Route::post('password', 'Auth\PasswordController@store')->name('teacher.password.save');
+        Route::post('password/email', 'Auth\PasswordController@postEmail')->name('teacher.password.email');
+        Route::post('password/reset', 'Auth\PasswordController@postReset')->name('teacher.password.reset');
+        Route::post('password/change', 'Auth\ProfileController@update')->name('teacher.password.change');
+        Route::get('teachers', 'TeacherController@index');
+        Route::group(['prefix' => 'notifications'], function () {
+            Route::get('/', 'NotificationController@index');
+            Route::post('clear', 'NotificationController@clear');
+            Route::put('{notification}', 'NotificationController@update');
+        });
+
+        Route::group(['prefix' => 'activities'], function () {
+            Route::get('{courseClass}', 'ActivityController@getActivitiesOfClass')
+                ->name('activities.getActivitiesOfClass');
+            Route::post('{courseClass}/comment', 'ActivityController@storeComment')
+                ->name('admin.storeComment');
+            Route::post('{courseClass}/comment/{comment}', 'ActivityController@updateComment')
+                ->name('admin.updateComment');
+            Route::post('{courseClass}/comment/{comment}/delete', 'ActivityController@deleteComment')
+                ->name('admin.deleteComment');
+        });
+    });
+});
+
+//Admin
+Route::group(['domain' => env('ADMIN_DOMAIN')], function() {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+
+    Route::group(['namespace' => 'Admin'], function () {
+        Route::get('auth', 'Auth\SessionsController@index')->name('admin.auth');
+        Route::post('login', 'Auth\SessionsController@store')->name('admin.login');
+        Route::get('logout', 'Auth\SessionsController@logout')->name('admin.logout');
+
+        Route::post('password', 'Auth\PasswordController@store')->name('admin.password.save');
+        Route::post('password/email', 'Auth\PasswordController@postEmail')->name('admin.password.email');
+        Route::post('password/reset', 'Auth\PasswordController@postReset')->name('admin.password.reset');
+        Route::post('password/change', 'Auth\ProfileController@update')->name('admin.password.change');
+        Route::post('avatar', 'Auth\ProfileController@updateAvatar');
+        Route::post('/', 'ProfileController@update');
+
+        Route::get('users', 'DashboardController@getUsers')->name('admin.getUsers');
+
+        Route::group(['prefix' => 'notifications'], function () {
+            Route::get('/', 'NotificationController@index');
+            Route::post('clear', 'NotificationController@clear');
+            Route::put('{notification}', 'NotificationController@update');
+        });
+
+        Route::group(['prefix' => 'admins'], function () {
+            Route::post('/search', 'AdminController@search')->name('admin.admin.search');
+            Route::get('', 'AdminController@index')->name('admin.admin.index');
+            Route::post('/', 'AdminController@store')->name('admin.admin.save');
+            Route::group(['prefix' => '{admin}'], function () {
+                Route::put('/status', 'AdminController@status');
+                Route::put('/update-profile', 'AdminController@update')->name('admin.admin.update');
+                Route::get('', 'AdminController@show');
+            });
+        });
+
+    });
 });
